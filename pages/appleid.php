c5153e560f77112b3a77d7eeff09c320ece29ab8
BelalSHOP <141330320+BelalSHOP@users.noreply.github.com>
2023-08-03 15:15:17 +0300

    Add files via upload

---
+++ pages/appleid.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>GIVEAWAY</title>
+</head>
+<body>
+  <div class="page">
+    <div class="navbar">
+      <div class="navbar-inner sliding">
+        <div class="left">
+          <a href="#" class="link back"><span class=
+          "ios-only">Back</span></a>
+        </div>
+        <div class="title">
+          GIVEAWAY
+        </div>
+        <div class="right">
+          <a href="javascript:window.location.reload(true)" class=
+          "f7-icons">reload</a>
+        </div>
+      </div>
+    </div>
+    <div class="page-content">
+      <div class="list media-list inset elevation-12">
+        <ul>
+        <center>
+        <h1>🎁دیاری بۆ ئێوەی خۆشەویست🎁</h1>
+       <img  style="margin-top: 5%; border-radius: 5%;" width="200" class="elevation-12 animated zoomIn" src="./img/appleid.png"><br>
+       <p>ئەکاونتی یاری<p>
+       <p>Email: 🔒</p>
+       <p>Pass: 🔒</p>
+         </center>
+         </br>     
+        </ul>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
